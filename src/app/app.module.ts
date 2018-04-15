87eeb65387e97f663a9810f78ff5fc29c8e81557
Enrique Juan de Dios <juandedioz@gmail.com>
2018-04-15 18:20:41 -0500

    Added Home Component and Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
+const AppRouter: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRouter,{enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
