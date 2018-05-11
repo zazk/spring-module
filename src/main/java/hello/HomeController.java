08f1d26ce6729b8c3e2af6bf4ee8106f64014b90
Joel <jim.leonel11@gmail.com>
2018-05-11 14:32:42 -0500

    listas

--- src/main/java/hello/HomeController.java
+++ src/main/java/hello/HomeController.java
@@ -35,6 +35,61 @@ public class HomeController {
                 System.out.println( listRepository.showListJdbc() );
                 return listRepository.showListJdbc();
         }
+
+        // Lista las rutas
+        @RequestMapping(value = "/list_rutas", method = RequestMethod.GET, produces = "application/json")
+        @ResponseBody
+        public List<Map<String, Object>> list_rutas()  {
+                        //Get from Query
+                System.out.println( listRepository.showListRutas() );
+                return listRepository.showListRutas();
+        }
+
+        // Lista las categorias
+        @RequestMapping(value = "/list_categorias", method = RequestMethod.GET, produces = "application/json")
+        @ResponseBody
+        public List<Map<String, Object>> list_categorias()  {
+                        //Get from Query
+                System.out.println( listRepository.showListCategorias() );
+                return listRepository.showListCategorias();
+        }
+
+        // Lista los paises
+        @RequestMapping(value = "/list_paises", method = RequestMethod.GET, produces = "application/json")
+        @ResponseBody
+        public List<Map<String, Object>> list_paises()  {
+                        //Get from Query
+                System.out.println( listRepository.showListPaises() );
+                return listRepository.showListPaises();
+        }
+
+        // Lista los tipos de documentos
+        @RequestMapping(value = "/list_tipdocumentos", method = RequestMethod.GET, produces = "application/json")
+        @ResponseBody
+        public List<Map<String, Object>> list_tipdocumentos()  {
+                        //Get from Query
+                System.out.println( listRepository.showListTipdocumentos() );
+                return listRepository.showListTipdocumentos();
+        }
+
+        // Lista las noticias
+        @RequestMapping(value = "/list_noticias", method = RequestMethod.GET, produces = "application/json")
+        @ResponseBody
+        public List<Map<String, Object>> list_noticias()  {
+                        //Get from Query
+                System.out.println( listRepository.showListNoticias() );
+                return listRepository.showListNoticias();
+        }
+
+        // Lista las noticias activas
+        @RequestMapping(value = "/list_noticias_activas", method = RequestMethod.GET, produces = "application/json")
+        @ResponseBody
+        public List<Map<String, Object>> list_noticias_activas()  {
+                        //Get from Query
+                System.out.println( listRepository.showListNoticiasActivas() );
+                return listRepository.showListNoticiasActivas();
+        }
+
         
         @RequestMapping(value = "/get-books", method = RequestMethod.GET, produces = "application/json")
         @ResponseBody
--- src/main/java/hello/ListRepository.java
+++ src/main/java/hello/ListRepository.java
@@ -31,8 +31,44 @@ public class ListRepository {
     }
 
     public List<Map<String, Object>> showListJdbc(){
-		List<Map<String, Object>> list =  jdbcTemplate.queryForList("SELECT * from routes "); 
+		List<Map<String, Object>> list =  jdbcTemplate.queryForList("SELECT * FROM t_categoria"); 
     	return list;
+    }
+
+    // lista las rutas
+        public List<Map<String, Object>> showListRutas(){
+        List<Map<String, Object>> list_rutas =  jdbcTemplate.queryForList("SELECT * FROM t_ruta"); 
+        return list_rutas;
+    }
+
+    // lista las categorias
+        public List<Map<String, Object>> showListCategorias(){
+        List<Map<String, Object>> list_categorias =  jdbcTemplate.queryForList("SELECT * FROM t_categoria"); 
+        return list_categorias;
+    }
+
+    // lista los paises
+        public List<Map<String, Object>> showListPaises(){
+        List<Map<String, Object>> list_paises =  jdbcTemplate.queryForList("SELECT * FROM t_pais"); 
+        return list_paises;
+    }
+
+    // lista los documentos
+        public List<Map<String, Object>> showListTipdocumentos(){
+        List<Map<String, Object>> list_tipdocumentos =  jdbcTemplate.queryForList("SELECT * FROM t_tip_documento"); 
+        return list_tipdocumentos;
+    }
+
+    // lista las noticias
+        public List<Map<String, Object>> showListNoticias(){
+        List<Map<String, Object>> list_noticias =  jdbcTemplate.queryForList("SELECT * FROM t_noticia"); 
+        return list_noticias;
+    }
+
+    // lista las noticias activas
+        public List<Map<String, Object>> showListNoticiasActivas(){
+        List<Map<String, Object>> list_noticias_activas =  jdbcTemplate.queryForList("SELECT * FROM t_noticia WHERE bol_activo='1'"); 
+        return list_noticias_activas;
     }
     
     public List<Map<String, Object>> showListStoreProcedures(){
